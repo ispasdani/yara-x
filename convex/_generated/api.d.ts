8518ec5f20aee7e328b3dc03896b932b0b08fd08
ispasdani <ispasdani14@gmail.com>
2025-08-08 12:12:47 +0200

    updated api

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,12 +13,19 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as chatHistories from "../chatHistories.js";
 import type * as contractAnalyses from "../contractAnalyses.js";
 import type * as contractAnalyzeGemini from "../contractAnalyzeGemini.js";
 import type * as creditTransactions from "../creditTransactions.js";
+import type * as documentAnalyses from "../documentAnalyses.js";
+import type * as documentShares from "../documentShares.js";
 import type * as http from "../http.js";
+import type * as organizationMember from "../organizationMember.js";
+import type * as organizations from "../organizations.js";
+import type * as researchResults from "../researchResults.js";
 import type * as schemas from "../schemas.js";
 import type * as tasks from "../tasks.js";
+import type * as templates from "../templates.js";
 import type * as users from "../users.js";
 
 /**
@@ -30,12 +37,19 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  chatHistories: typeof chatHistories;
   contractAnalyses: typeof contractAnalyses;
   contractAnalyzeGemini: typeof contractAnalyzeGemini;
   creditTransactions: typeof creditTransactions;
+  documentAnalyses: typeof documentAnalyses;
+  documentShares: typeof documentShares;
   http: typeof http;
+  organizationMember: typeof organizationMember;
+  organizations: typeof organizations;
+  researchResults: typeof researchResults;
   schemas: typeof schemas;
   tasks: typeof tasks;
+  templates: typeof templates;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
