807083fe40c17c0ac02025462c0675a8f1c19edb
ispasdani <ispasdani14@gmail.com>
2025-08-08 15:26:10 +0200

    language switcher test

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { api } from "../convex/_generated/api";
+import { LanguageSelector } from "@/components/LanguageSelector";
+import { usePersistedLanguageStore } from "@/stores/languageStore";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
+  const currentLanguage = usePersistedLanguageStore(
+    (state) => state.currentLanguage
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+      <LanguageSelector />
+
+      <p>Test: {currentLanguage}</p>
     </main>
   );
 }
