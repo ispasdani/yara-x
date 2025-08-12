0f20ea6c5cc8059081a174ecea7b4308c67f78f1
ispasdani <ispasdani14@gmail.com>
2025-08-12 23:13:19 +0200

    language hook added

---
+++ hooks/useLanguageData.ts
@@ -0,0 +1,27 @@
+// hooks/useLanguageData.ts
+import { useEffect, useState } from "react";
+import { usePersistedLanguageStore } from "@/stores/languageStore";
+import { LanguageData } from "@/types/languageDataTypes";
+
+export const useLanguageData = () => {
+  const { currentLanguage } = usePersistedLanguageStore();
+  const [langData, setLangData] = useState<LanguageData | null>(null);
+
+  useEffect(() => {
+    const loadLanguageData = async () => {
+      try {
+        const dataModule = await import(`@/consts/${currentLanguage}.ts`);
+        setLangData(
+          dataModule.default[currentLanguage] || dataModule.default.en
+        );
+      } catch (error) {
+        // Fallback to Romanian (or English) if the language file fails to load
+        const ro = await import(`@/consts/ro`);
+        setLangData(ro.default.ro);
+      }
+    };
+    loadLanguageData();
+  }, [currentLanguage]);
+
+  return { langData, currentLanguage };
+};
