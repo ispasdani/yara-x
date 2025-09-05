433a385d826ba3769fd3a0ffae2954f5fd473427
ispasdani <ispasdani14@gmail.com>
2025-09-05 23:41:01 +0200

    updated text

--- consts/ro/public/hero.ts
+++ consts/ro/public/hero.ts
@@ -1,8 +1,9 @@
 const hero = {
     floatingMessage: {title: "Automatizare juridică cu AI"},
-    heroTitle: {title: "Legea, simplificată cu AI"},
-    heroDescription: {title: "Transformați practica juridică cu revizuirea documentelor asistată de AI, cercetare inteligentă și fluxuri de lucru automatizate fără a compromite precizia."},
-    heroChatPlaceholder: {title: "Întreabă-mă orice despre lege sau incarca un document pentru analiza..."},
+    title: {title: "Legea, simplificată cu AI"},
+    description: {title: "Transformați practica juridică cu revizuirea documentelor asistată de AI, cercetare inteligentă și fluxuri de lucru automatizate fără a compromite precizia."},
+    chatPlaceholder: {title: "Întreabă-mă orice despre lege sau incarca un document pentru analiza..."},
+    chatGuidelines: {title: "„Apasă Enter pentru a trimite • Shift+Enter pentru rând nou"},
 };
 
 export default hero;
